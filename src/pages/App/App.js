e25a1c1becda3c0e8a240b146b60fb71fdf1e1da
Christopher Violante <c.violante211@gmail.com>
2020-10-26 14:22:45 -0600

    organize and trim files from react create

---
+++ src/pages/App/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return(
+      <>
+      <h1>TANDEM TRIVIA</h1>
+      </>
+    )
+  }
+}
+
+export default App;
