f0acaeb8b517b03dee1b4f86d282b98a0b0a9fa3
Christopher Violante <c.violante211@gmail.com>
2020-11-02 14:58:24 -0700

    intro on homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,17 @@ class HomePage extends Component{
     render() {
         return(
             <div>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum, ullam. Aut a sequi dolorem exercitationem laboriosam atque, veniam provident ipsam, necessitatibus, repellat nulla commodi cumque dolorum eveniet sed ad perferendis?</p>
+                <p>Tandem Trivia is a quick to play, fun game that was built using the 2020 Tandem Internship code challenge guidelines. The questions are scored jeopardy style meaning if you are wrong you lose a point and right gains a point.
+                </p><br></br>
+
+                <p>There are A LOT of trivia application tutorials on the internet as it is one of the relatively more basic things someone new to coding can build and have fun with. For this application I made a firm point to not use any code, any concepts, or even consult any of these walk throughs while building this game. All of the algorithms for question selection, question scoring, and gameplay were of my own design.
+                </p><br></br>
+
+                <p>This doesn't mean Tandem Trivia is the best trivia game out there, it means quite the opposite. The game is not perfect but the game did come from my own mind and I believe shows some of what I am capable of at this point in time.
+                </p><br></br>
+
+                <p>Press 'New Game' to start a 10 questions round of Trivia!<br></br>
+                </p>
                 <Link className='btn btn-default' to='/GamePage'>New Game</Link>
             </div>
         )
